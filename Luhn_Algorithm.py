5e10d5e33ce9686663e2171e4eb897dcb34a241d
carlogyannuzzi <carlogyannuzzi@gmail.com>
2024-02-13 13:08:19 +0000

    updated verify_card_number function to seperate odd from even numbers and summed the odd numbers.

--- Luhn_Algorithm.py
+++ Luhn_Algorithm.py
@@ -2,8 +2,21 @@
 
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
-    card_number_reversed = card_number[0:4:2]
-    print(card_number_reversed)
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
+
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+    
+    
+    sum_of_even_digits = 0
+
+    even_digits =  card_number_reversed[1:2]
+    for digit in even_digits:
+        number = int(digit) * 2
+        if number >= 10:
+            print(number) 
+    
 
 
 
@@ -13,7 +26,7 @@ def main():
     card_translation = str.maketrans({'-': '',' ': ''})
     translated_card_number = card_number.translate(card_translation)
 
-    print(translated_card_number)
+    
 
     verify_card_number(translated_card_number)
 
